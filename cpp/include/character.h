8b05dcd6338e9a6addfbf988c416ea831337e20c
dynamonda <v7gj9kk@gmail.com>
2018-05-24 06:04:34 +0900

    Add Character

---
+++ cpp/include/character.h
@@ -0,0 +1,11 @@
+#ifndef _INCLUDE_CHARACTER_H_
+#define _INCLUDE_CHARACTER_H_
+
+class Character{
+public:
+    Character(){};
+    
+private:
+};
+
+#endif
--- cpp/src/main.cc
+++ cpp/src/main.cc
@@ -1,3 +1,9 @@
+#include <iostream>
+#include "character.h"
+
 int main(int argc, char* argv[]){
+    std::cout << "Hello World\n";
+    Character ch;
+     
     return 0;
 }
