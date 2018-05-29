efe493f539ddb0e6751a9e499d4c8d3040fd5178
dynamonda <v7gj9kk@gmail.com>
2018-05-29 14:03:37 +0900

    Add Gender enum class in Character

--- cpp/include/character.h
+++ cpp/include/character.h
@@ -1,11 +1,17 @@
 #ifndef _INCLUDE_CHARACTER_H_
 #define _INCLUDE_CHARACTER_H_
 
+enum class Gender{
+    Male,
+    Female
+};
+
 class Character{
 public:
-    Character(){};
+    Character();
     
 private:
+    Gender gender_;
 };
 
 #endif
