4309ff77a622f43b271c9850bf30ac64772732c3
dynamonda <v7gj9kk@gmail.com>
2019-12-19 04:25:12 +0900

    SQLite3 Addition

---
+++ cpp/include/databaseManager.hpp
@@ -0,0 +1,22 @@
+#ifndef _CHRONICA_CPP_INCLUDE_DATABASEMANAGER_HPP_
+#define _CHRONICA_CPP_INCLUDE_DATABASEMANAGER_HPP_
+
+#include "sqlite3.h"
+#include <iostream>
+
+const char* const dbname_ = "../../db.sqlite3";
+sqlite3 *pdb = nullptr;
+
+class DatabaseManager{
+public:
+    DatabaseManager()
+    {
+        int err = sqlite3_open(dbname_, &pdb);
+        if(err){
+            printf("OPEN ERROR\n");
+        }
+        printf("OPEN SUCCESS\n");
+    }
+};
+
+#endif
--- cpp/src/main.cc
+++ cpp/src/main.cc
@@ -2,12 +2,14 @@
 #include "character.h"
 #include "api.h"
 #include "lua_loader.hpp"
+#include "databaseManager.hpp"
 
 int main(int argc, char* argv[]){
     std::cout << "Hello World\n";
     
     std::unique_ptr<API> api_ = std::make_unique<API>();
     std::unique_ptr<LuaLoader> lua_ = std::make_unique<LuaLoader>();
-
+    std::unique_ptr<DatabaseManager> dbm = std::make_unique<DatabaseManager>();
+    
     return 0;
 }
